19093246ae3991cad0c31e00450159a48eb3e615
Dejan Totev <dejan.totev@psinteractive.io>
2021-01-18 22:21:30 +0200

    fix: wrong lint script

--- scripts/lint/lint.js
+++ scripts/lint/lint.js
@@ -1,47 +1,25 @@
-const { spawn } = require('helpers-fn')
-const { parse, resolve } = require('path')
-const { mapAsync } = require('rambdax')
+const { spawn, scanFolder } = require('helpers-fn')
+const { resolve } = require('path')
+const { mapAsync, remove } = require('rambdax')
 
 const base = resolve(__dirname, '../../../rambda/')
 
-const folders = [
-  'source',
-  'scripts/all-scripts',
-  'scripts/add-new-method',
-  'scripts/build-step',
-  'scripts/consume-typings',
-  'scripts/populate-docs-data',
-  'scripts/populate-readme-data',
-  'scripts/run-benchmarks',
-]
-
-const files = [
-  'rambda.js',
-  'scripts/run-ramda-specs/run-ramda-specs.js',
-  'scripts/run-ramda-specs/run-ramda-specs.spec.js',
-  'scripts/run-ramda-specs/import-ramda-specs.spec.js',
-  'scripts/run-ramda-specs/import-ramda-specs.js',
-]
-
-async function lintFolder(folder){
+async function lintFile(filePath){
+  const relativeFilePath = remove(base, filePath)
+  console.time(relativeFilePath)
   await spawn({
-    cwd     : `${ base }/${ folder }`,
+    cwd     : base,
     command : 'run',
-    inputs  : [ 'lint' ],
+    inputs  : [ 'lintfile', relativeFilePath ],
   })
-}
-
-async function lintFile(file){
-  const filePath = `${ base }/${ file }`
-  const { dir, name } = parse(filePath)
-  await spawn({
-    cwd     : dir,
-    command : 'run',
-    inputs  : [ 'lintfile', `${ name }.js` ],
-  })
+  console.timeEnd(relativeFilePath)
 }
 
 void async function lint(){
-  await mapAsync(lintFolder)(folders)
-  await mapAsync(lintFile)(files)
+  const sourceFiles = await scanFolder({ folder: `${base}/source`})
+  const allFiles = [
+    ...sourceFiles,
+    `${base}/rambda.js`
+  ]
+  await mapAsync(lintFile, allFiles)
 }()
