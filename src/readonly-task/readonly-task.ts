f581b8afeadc24ae1992709c2399f7717144c02d
Dejan Totev <dejan.totev@psinteractive.io>
2021-06-19 11:33:05 +0300

    feat: readonly task

--- src/readonly-task/readonly-task.ts
+++ src/readonly-task/readonly-task.ts
@@ -1,7 +1,11 @@
-import { mapAsync , filter } from 'rambdax'
-import got from 'got'
-import { log } from 'helpers-fn'
-
+import { replace } from 'rambdax'
+import {writeFile, readFile } from 'fs-extra'
+import { PATHS } from '../constants'
 
 export async function readonlyTask(){
+  const filePath = `${PATHS.base}/index.d.ts`
+  const output = `${PATHS.base}/readonly.ts`
+  const content = (await readFile(filePath)).toString()
+  const withoutReadonly = replace(/readonly\s/g, '', content)
+  await writeFile(output, withoutReadonly)
 }
