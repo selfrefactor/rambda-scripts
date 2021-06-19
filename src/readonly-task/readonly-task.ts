3f86d8698b57baf687e342a8ac0b8f584306dfeb
Dejan Totev <dejan.totev@psinteractive.io>
2021-06-19 11:46:58 +0300

    fix: wrong output for readonly task

--- src/readonly-task/readonly-task.ts
+++ src/readonly-task/readonly-task.ts
@@ -4,7 +4,7 @@ import { PATHS } from '../constants'
 
 export async function readonlyTask(){
   const filePath = `${PATHS.base}/index.d.ts`
-  const output = `${PATHS.base}/readonly.ts`
+  const output = `${PATHS.base}/non-strict.ts`
   const content = (await readFile(filePath)).toString()
   const withoutReadonly = replace(/readonly\s/g, '', content)
   await writeFile(output, withoutReadonly)
