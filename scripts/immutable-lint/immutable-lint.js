2df33ef623c3dd0cd154cbfa9fd0a15458df2a61
Dejan Totev <dejan.totev@psinteractive.io>
2021-07-24 15:37:01 +0300

    feat: rambdax immutable

--- scripts/immutable-lint/immutable-lint.js
+++ scripts/immutable-lint/immutable-lint.js
@@ -2,14 +2,16 @@ const { resolve } = require('path')
 const { exec, log } = require('helpers-fn')
 const { copy } = require('fs-extra')
 
+const WITH_RAMBDAX = process.env.WITH_RAMBDAX === 'ON'
+
 void async function immutableLint(){
   const filePath = resolve(
     __dirname,
-    '../../../rambda/immutable.d.ts'
+    `../../../${WITH_RAMBDAX ? 'rambdax' : 'rambda'}/immutable.d.ts`
   )
   const filePathOrigin = resolve(
     __dirname,
-    '../../../rambda/index.d.ts'
+    `../../../${WITH_RAMBDAX ? 'rambdax' : 'rambda'}/index.d.ts`
   )
   await copy(filePathOrigin, filePath)
   const cwd = resolve(
