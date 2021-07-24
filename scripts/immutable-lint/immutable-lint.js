c8f4cb187d1c857ba425b9bdaccdbd6d494e9922
Dejan Totev <dejan.totev@psinteractive.io>
2021-07-24 22:00:51 +0300

    chore@dnd

--- scripts/immutable-lint/immutable-lint.js
+++ scripts/immutable-lint/immutable-lint.js
@@ -1,29 +1,36 @@
-const { resolve } = require('path')
-const { exec, log } = require('helpers-fn')
-const { copy } = require('fs-extra')
+const {resolve} = require('path')
+const {exec, log} = require('helpers-fn')
+const {copy, outputFile} = require('fs-extra')
 
 const WITH_RAMBDAX = process.env.WITH_RAMBDAX === 'ON'
 
-void async function immutableLint(){
+function getJsContent(withRambdax) {}
+
+void (async function immutableLint() {
   const filePath = resolve(
     __dirname,
     `../../../${WITH_RAMBDAX ? 'rambdax' : 'rambda'}/immutable.d.ts`
+  )
+  const jsFilePath = resolve(
+    __dirname,
+    `../../../${WITH_RAMBDAX ? 'rambdax' : 'rambda'}/immutable.js`
   )
   const filePathOrigin = resolve(
     __dirname,
     `../../../${WITH_RAMBDAX ? 'rambdax' : 'rambda'}/index.d.ts`
   )
-  await copy(filePathOrigin, filePath)
-  const cwd = resolve(
-    __dirname,
-    '../../'
+  await outputFile(
+    jsFilePath,
+    `module.exports = require('./dist/${WITH_RAMBDAX ? 'rambdax' : 'rambda'}.js')`
   )
+  await copy(filePathOrigin, filePath)
+  const cwd = resolve(__dirname, '../../')
 
-  const command= `node node_modules/eslint/bin/eslint.js -c files/.eslint.config.js ${filePath} --fix`
+  const command = `node node_modules/eslint/bin/eslint.js -c files/.eslint.config.js ${filePath} --fix`
 
   await exec({
     cwd,
-    command
+    command,
   })
   log('success', 'big')
-}()
+})()
