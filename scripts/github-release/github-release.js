5a2fc828198f0150b755062cbd10f2c2d58160b5
Dejan Totev <dejan.totev@psinteractive.io>
2022-01-02 11:15:05 +0200

    service: github tag for rambdax

--- scripts/github-release/github-release.js
+++ scripts/github-release/github-release.js
@@ -3,7 +3,8 @@ const {existsSync} = require('fs')
 const {exec} = require('helpers-fn')
 const { readJson } = require('fs-extra')
 
-const BASE = resolve(__dirname, '../../../rambda')
+const library = process.env.WITH_RAMBDAX ? 'rambdax':'rambda'
+const BASE = resolve(__dirname, `../../../${library}`)
 const PACKAGE_JSON = `${BASE}/package.json` 
 
 void async function githubRelease(){
