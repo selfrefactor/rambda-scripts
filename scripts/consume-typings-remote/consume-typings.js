0137e10bcd5e76a93e296bd68e1f7148f1cd2a5f
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-03 17:15:09 +0300

    chore@small

---
+++ scripts/consume-typings-remote/consume-typings.js
@@ -0,0 +1,39 @@
+"use strict";
+exports.__esModule = true;
+// import { add, applySpec, reject, and, transpose, move, union, reduce } from 'rambda/immutable'
+var rambdax_1 = require("rambdax");
+// import {sortByProps} from 'rambdax'
+// "rambda": "file:./../../../rambda/",
+// const sortByPropsResult = sortByProps(['a.b', 'a.c'], [
+//   {a: {b: 2, c: 4}},
+//   {a: {b: 2, c: 3}},
+// ])
+var moveResult = (0, rambdax_1.move)(1, 2, [1, 2, 3]);
+var unionResult = (0, rambdax_1.union)([1, 2, 4], [1, 2, 3]);
+var applySpecResult = (0, rambdax_1.applySpec)({
+    a: (0, rambdax_1.add)(1)
+})(1);
+var transposeResult = (0, rambdax_1.transpose)([[1, 2], [], [1, 2, 3], [3]]);
+console.log({ applySpecResult: applySpecResult, transposeResult: transposeResult, moveResult: moveResult, unionResult: unionResult });
+// console.log({sortByPropsResult: sortByPropsResult[0]})
+var bs = (0, rambdax_1.and)(1)(2);
+var a = (0, rambdax_1.reject)(function (a) { return a > 1; }, [1, 2, 3]);
+function fn(input) {
+    return input.c ? input.a : input.b;
+}
+var foo = {
+    bar: ['1', '2', '3']
+};
+var numberArray = [1, 2, 3];
+var result = (0, rambdax_1.reduce)(function (acc, elem, i) {
+    acc; // $ExpectType number
+    elem; // $ExpectType number
+    i; // $ExpectType number
+    return acc + elem;
+}, 1, numberArray);
+// const curried = partialCurry<Input, PartialInput, string|number>(fn, {a:1, b:'foo'});
+// curried // $ExpectType (input: Pick<Input, "c">) => string | number
+// const result = curried({c:false})
+// result// $ExpectType string | number
+// const partialInput = {a:1}
+// type B = Exclude<keyof Input, keyof PartialInput>
---
+++ scripts/consume-typings-remote/consume-typings.ts
@@ -0,0 +1,64 @@
+// import { add, applySpec, reject, and, transpose, move, union, reduce } from 'rambda/immutable'
+import { add, applySpec, reject, and, transpose, move, union, reduce } from 'rambdax'
+// import {sortByProps} from 'rambdax'
+// "rambda": "file:./../../../rambda/",
+
+// const sortByPropsResult = sortByProps(['a.b', 'a.c'], [
+//   {a: {b: 2, c: 4}},
+//   {a: {b: 2, c: 3}},
+// ])
+
+const moveResult = move(1,2, [1,2,3])
+const unionResult = union([1,2,4], [1,2,3])
+
+const applySpecResult = applySpec({
+  a: add(1)
+})(1)
+
+const transposeResult = transpose([[1,2],[],[1,2,3],[3]])
+console.log({applySpecResult, transposeResult, moveResult, unionResult})
+// console.log({sortByPropsResult: sortByPropsResult[0]})
+const bs = and(1)(2)
+
+const a = reject((a)=> a > 1, [1,2,3])
+
+interface Input{
+  a: number
+  b: string
+  c: boolean
+}
+
+type PartialInput = Pick<Input, Exclude<keyof Input, "c">>;
+function fn(input: Input){
+  return input.c ? input.a: input.b
+}
+
+interface Foo {
+  [key: string]: string[]
+}
+
+const foo: Foo = {
+  bar: ['1', '2', '3'],
+}
+
+const numberArray = [1, 2, 3];
+
+const result = reduce(
+  (acc, elem, i) => {
+    acc // $ExpectType number
+    elem // $ExpectType number
+    i // $ExpectType number
+    return acc + elem
+  },
+  1,
+  numberArray
+)
+
+// const curried = partialCurry<Input, PartialInput, string|number>(fn, {a:1, b:'foo'});
+// curried // $ExpectType (input: Pick<Input, "c">) => string | number
+
+// const result = curried({c:false})
+// result// $ExpectType string | number
+
+// const partialInput = {a:1}
+// type B = Exclude<keyof Input, keyof PartialInput>
