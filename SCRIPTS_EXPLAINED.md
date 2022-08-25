25acbf21baa322ba9a3ea12c2926400c236a662e
Deyan Totev <deyan.totev.extern@seven.one>
2022-08-25 09:11:46 +0300

    chore@small

--- SCRIPTS_EXPLAINED.md
+++ SCRIPTS_EXPLAINED.md
@@ -1,3 +1,7 @@
 ## bench:sample
 
 run benchmarks in isolated mode, i.e. without being part of Rambda tooling.
+
+## tree-shaking
+
+Prove Rambda is tree-shakable
---
+++ scripts/tree-shaking/tree-shaking.js
@@ -0,0 +1,13 @@
+const getFolderSize = require('get-folder-size')
+const { resolve } = require('path')
+const { exec } = require('helpers-fn')
+// const { resolve } = require('fs-extra')
+
+const webpackDir = resolve(__dirname,'../tree-shaking-webpack' )
+const angularDir = resolve(__dirname,'../tree-shaking-angular' )
+const viteDir = resolve(__dirname,'../tree-shaking-vite' )
+// confirm to update values
+
+void async function treeShaking(){
+  console.log(webpackDir, `webpackDir`)
+}()
