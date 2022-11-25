dc6d229324e37eb800de9a7d2761c655eea1763b
Deyan Totev <deyan.totev.extern@seven.one>
2022-11-25 17:40:00 +0200

    chore@small

--- scripts/compare-size/compare-size.js
+++ scripts/compare-size/compare-size.js
@@ -4,7 +4,6 @@ const {exec} = require('helpers-fn')
 
 const allMethodx = Object.keys(Ramda)
 const allMethods = Object.keys(R).filter(x => allMethodx.includes(x)).join(' ')
-console.log(allMethods, `allMethod`)
 let command = `npm run --silent partial-build ${allMethods} > dist.ramda.custom.js`
 
 exec({command, cwd: __dirname})
