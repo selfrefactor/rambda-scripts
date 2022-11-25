dc99cee2be3d0e2bac6900862e09a20b668050a9
Deyan Totev <deyan.totev.extern@seven.one>
2022-11-25 17:49:18 +0200

    chore@small

--- scripts/compare-size/compare-size.js
+++ scripts/compare-size/compare-size.js
@@ -1,9 +1,20 @@
 const R = require('rambda')
 const Ramda = require('ramda')
 const {exec} = require('helpers-fn')
+const { interpolate } = require('rambdax')
 
 const allMethodx = Object.keys(Ramda)
 const allMethods = Object.keys(R).filter(x => allMethodx.includes(x)).join(' ')
+const allMethodsxx = Object.keys(R).filter(x => allMethodx.includes(x)).map(x => `export const ${x} = R.${x};`).join('\n')
 let command = `npm run --silent partial-build ${allMethods} > dist.ramda.custom.js`
 
-exec({command, cwd: __dirname})
+// exec({command, cwd: __dirname})
+
+let template = `
+import * as R from 'ramda';
+
+{{allMethods}} 
+`
+
+const content = interpolate(template, {allMethods: allMethodsxx})
+console.log(content, `content`)
