b9dce1683ef6d3bbbb4c5c08864c5eb04cec8ebc
Deyan Totev <deyan.totev.extern@seven.one>
2023-05-24 15:55:32 +0300

    chore@small

--- scripts/consume-typings/consume-typings-demo.js
+++ scripts/consume-typings/consume-typings-demo.js
@@ -1,16 +1,37 @@
 "use strict";
 exports.__esModule = true;
+exports.first = exports.lastOf = void 0;
 // import * as R from 'remeda'
 var R = require("rambda");
 var Ramda = require("ramda");
-// const list = [ 1, 2, 3, 'foo' ]
-var list = [1, 2, 3, 'foo'];
-var listx = [1, 2, 3, 'foo'];
+function lastOf(array) {
+    return array[array.length - 1];
+}
+exports.lastOf = lastOf;
+// export function last<T extends unknown[]>(input: T): T extends [...infer _, infer U] ? U : T[0]
+function first(array) {
+    return array[0];
+}
+exports.first = first;
+var list = ['bar', 1, 2, 3, 'foo'];
+var listx = ['bar', 1, 2, 3, 'foo'];
+var a1 = first(list);
+var a2 = first(listx);
 var a = Ramda.last(list);
-var aa = R.last(listx);
+var xa = Ramda.last(listx);
+var aa = R.last(list);
+var aax = R.last(listx);
+var aay = lastOf(listx);
+var asd = lastOf(list);
 var bb = list[0];
 console.log({
+    a1: a1,
+    a2: a2,
     a: a,
+    asd: asd,
+    aax: aax,
+    aay: aay,
+    xa: xa,
     aa: aa,
     bb: bb
 });
--- scripts/consume-typings/consume-typings-demo.ts
+++ scripts/consume-typings/consume-typings-demo.ts
@@ -14,19 +14,27 @@ type LastArrayElement<ValueType extends readonly unknown[]> =
 export function lastOf<V extends readonly any[]>(array: V): LastArrayElement<V>{
 	return array[array.length - 1];
 }
+// export function last<T extends unknown[]>(input: T): T extends [...infer _, infer U] ? U : T[0]
 
+export function first<T extends unknown[]>(array: T): T extends [...infer _, infer U] ? U : T[0]{
+	return array[0] as any
+}
 
-// const list = [ 1, 2, 3, 'foo' ]
-const list = [ 1, 2, 3,'foo' ] as const
-const listx = [ 1, 2, 3,'foo' ]
+const list = [ 'bar',1, 2, 3,'foo' ] as const
+const listx = [ 'bar', 1, 2, 3,'foo' ]
+
+let a1 = first(list as any)
+let a2 = first(listx)
 let a = Ramda.last(list)
 let xa = Ramda.last(listx)
-let aa = R.last(list)
+let aa = R.last(list as any)
 let aax = R.last(listx)
 let aay = lastOf(listx)
 let asd = lastOf(list)
 let bb: LastArrayElement<typeof listx> = list[0]
 console.log( {
+  a1,
+  a2,
 	a,
 		asd,
 		aax,
