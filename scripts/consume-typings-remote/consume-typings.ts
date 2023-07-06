6003b711e201a35d6944afb4fb8ef5fa941bf0ca
Deyan Totev <deyan.totev.extern@seven.one>
2023-07-06 18:01:34 +0300

    chore@small

--- scripts/consume-typings-remote/consume-typings.ts
+++ scripts/consume-typings-remote/consume-typings.ts
@@ -1,9 +1,17 @@
-import { add, applySpec,  transpose, move, union, path, propEq } from 'rambda'
-import {propEq as propEqR} from 'ramda'
+import { add, applySpec,  transpose, move, union, path, propEq, sortBy, prop, map } from 'ramda'
+// import { add, applySpec,  transpose, move, union, path, propEq, sortBy, prop, map } from 'rambda'
 const alois = {name: 'Alois', age: 15, disposition: 'surly'};
  const hasBrownHair = propEq('brown', 'age', alois);
  const age = propEq(15, 'age', alois);
 console.log({hasBrownHair, age})
+const arr = [{ id: 1 }, { id: 2 }, { id: 3 }];
+
+const sortWithRProp = sortBy(prop('id'), arr);
+const sortWithoutRProp = sortBy((item) => item.id, arr);
+
+const mapWithRProp = map(prop('id'), arr);
+const mapWithoutRProp = map((item) => item.id, arr);
+
 process.exit(0)  
 const moveResult = move(1,2, [1,2,3])
 const unionResult = union([1,2,4], [1,2,3])
