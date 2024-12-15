e3e7584d6d59c2419a358fc1da3c61a3c3459ccb
Dejan Toteff <deyan8284@gmail.com>
2024-12-15 15:25:48 +0200

    chore@small

--- scripts/consume-deno/consume-deno.ts
+++ scripts/consume-deno/consume-deno.ts
@@ -1,20 +1,15 @@
-// import * as Ramda from "https://x.nest.land/ramda@0.27.2/mod.ts";
-// import * as R from 'https://deno.land/x/rambdax@11.2.6/mod.ts'
-import * as R from 'https://deno.land/x/rambda@9.4.1/mod.ts'
-// import * as R from 'https://deno.land/x/rambda/mod.ts'
-// import * as R from 'https://deno.land/x/rambda@v9.1.0/mod.ts'
-
-// let a=R.add(1)(2) // => will trigger warning in VSCode
-// console.log({a})
-// Ramda.add(1)('foo') // => will not trigger warning in VSCode
-// import * as R from "https://x.nest.land/rambda@8.3.0/mod.ts";
+// import * as R from 'https://deno.land/x/rambda@9.4.1/mod.ts'
+import * as R from 'https://deno.land/x/rambdax@11.3.0/mod.ts'
 
 const rEquals = R.equals(
-  {
-    a: {b: 1},
+	{
+		a: {b: 1},
   },
   {
-    a: {b: 1},
+		a: {b: 1},
   }
 )
 console.log(rEquals)
+
+// import * as R from 'https://deno.land/x/rambda/mod.ts'
+// import * as R from 'https://deno.land/x/rambda@v9.1.0/mod.ts'
